d2acf44a696bb9679d10a7f20a3a9fb454d7dcd1
David Miller <dmiller2117@gmail.com>
2023-09-04 23:21:30 +0100

    Adding a topic programmatically

---
+++ src/main/java/com/learnkafka/config/AutoCreateConfig.java
@@ -0,0 +1,36 @@
+package com.learnkafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+/**
+ * KafkaAdmin
+ * <ul>
+ * <li>Create topics programmatically</li>
+ * <li>Part of the SpringKafka</li>
+ * <li>How to create a topic from code?</li>
+ *  <ul>
+ *  <li>Create a bean of type KafkaAdmin in Spring configuration</li>
+ *  <li>Create a bean of type NewTopic in Spring configuration</li>
+ *  </ul>
+ * </ul>
+ */
+@Configuration
+public class AutoCreateConfig {
+
+    @Value("${spring.kafka.topic}")
+    String topicName;
+
+    @Bean
+    public NewTopic libraryEvents() {
+        return TopicBuilder
+                .name(topicName)
+                .partitions(3)
+                .replicas(3)
+                .build();
+    }
+
+}
