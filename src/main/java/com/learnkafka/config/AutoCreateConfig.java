10cbfab1801c78094701f97279066026b3570c6d
David Miller <dmiller2117@gmail.com>
2023-10-08 21:51:18 +0100

    Adding properties for ssl and run with 'ssl' profile

--- src/main/java/com/learnkafka/config/AutoCreateConfig.java
+++ src/main/java/com/learnkafka/config/AutoCreateConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 
 /**
@@ -19,6 +20,7 @@ import org.springframework.kafka.config.TopicBuilder;
  * </ul>
  */
 @Configuration
+@Profile("local")
 public class AutoCreateConfig {
 
     @Value("${spring.kafka.topic}")
